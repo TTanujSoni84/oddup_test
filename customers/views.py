67e0134eb02a3ea8e84f04ae9d7f5a0f46519dd8
Tanuj Soni <ttanujsoni84@gmail.com>
2021-02-01 17:50:34 +0530

    refactor: used update function for thread safety

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db import transaction
+from django.db.models import F
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
@@ -44,24 +45,30 @@ class TransactionCreateView(CreateAPIView):
     # if one of the queries fail
     @transaction.atomic
     def create(self, request):
-        
+
         serializer = self.get_serializer(data=request.data)
 
         # validating user data
         if serializer.is_valid():
             customer_id = request.data.get('customer')
+
+            # This object is necessary to check the balance before deduction
             customer = Customer.objects.get(pk=customer_id)
 
             # check whether a customer has enough account balance to
             # make this purchase
             if customer.account_balance < request.data.get('price'):
                 return Response("The customer doesn't have enough balance", status=status.HTTP_400_BAD_REQUEST)
+
             self.perform_create(serializer)
 
             # update the customer's balance post purchase
-            customer.account_balance -= request.data.get('price')
-            customer.save()
+            # TODO: Add database level check to prevent negative values
+            # Using update here works in tandem with @transaction.atomic in order to maintain
+            # thread safety
+            Customer.objects.filter(pk=customer_id).update(account_balance=F(
+                'account_balance')-request.data.get('price'))
+
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
